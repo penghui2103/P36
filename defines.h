a68f3a635e074f7f792e81c3338a2078fc3866b7
penghui <penguin.lovely@hotmail.com>
2010-05-24 17:24:25 +0800

    bacup changes to encode_new.c .h, subband.c.h, toolame.c audio_read.c defines.h

---
+++ defines.h
@@ -0,0 +1,23 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+
+/* datatypes for fixed point mp2 */
+#define INT8	char
+#define INT16	short
+#define INT32	int
+#define INT64	long long
+
+#define UINT8	unsigned char
+#define UINT16	unsiged short
+#define UINT32	unsigned int
+#define UINT64	unsigned int int
+
+
+#define WIN_COEFF_MUL 1e9
+#define WIN_COEFF_DIV 1e9
+
+#define DATA_SCALE	2^31
+
+
+#endif
